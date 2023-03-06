ce46f59c6381ae6bed9fa1fde5a438bba640c687
R2boyo25 <r2boyo25@gmail.com>
2023-03-05 23:03:20 -0600

    And the customize.el

--- config/customize.el
+++ config/customize.el
@@ -11,25 +11,24 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(custom-enabled-themes '(doom-ayu-dark))
  '(custom-safe-themes
    '("631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
  '(default
     ((t
       (:inherit nil :stipple nil :background "#ffffb1" :foreground "#141312" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 173 :width normal :foundry "monotype" :family "Iosevka Term Regular"))))
  '(package-selected-packages
-   '(doom-themes eglot mmm-mode multiple-cursors compat magit drag-stuff gited yaml lsp-mode rust-mode exwm))
+   '(org-journal doom-themes eglot mmm-mode multiple-cursors compat magit drag-stuff gited yaml lsp-mode rust-mode exwm))
  '(size-indication-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-button ((t nil)))
- '(custom-button-mouse ((t nil)))
- '(custom-button-pressed ((t nil)))
  '(dired-directory ((t (:foreground "medium sea green" :weight bold))))
  '(font-lock-comment-face ((t (:foreground "gray50"))))
  '(font-lock-keyword-face ((t (:foreground "#7230A9" :weight bold))))
  '(font-lock-type-face ((t (:foreground "DeepSkyBlue4" :weight bold))))
  '(line-number ((t (:inherit default :foreground "saddle brown" :slant italic :weight normal))))
- '(line-number-current-line ((t (:inherit (hl-line default) :foreground "peru" :slant italic :weight normal)))))
+ '(line-number-current-line ((t (:inherit (hl-line default) :foreground "peru" :slant italic :weight normal))))
+ '(org-agenda-date-today ((t (:foreground "sea green" :weight ultra-bold)))))
