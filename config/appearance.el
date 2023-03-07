f3bb9d00a699109b6ef7733bf315d98803de218a
R2boyo25 <r2boyo25@gmail.com>
2023-03-06 20:57:06 -0600

    Add .xprofile.example

--- config/appearance.el
+++ config/appearance.el
@@ -16,3 +16,7 @@
 
 (set-frame-parameter (selected-frame) 'alpha '(93 93))
 (add-to-list 'default-frame-alist '(alpha 93 93))
+
+;; Display time in mode-line
+(setq display-time-string-forms '((concat " " 24-hours ":" minutes " ")))
+(display-time-mode t)
