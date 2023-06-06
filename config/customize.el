cd6a460ca96474042765d1d4496654f6b00f35b2
R2Boyo25 <r2boyo25@gmail.com>
2023-06-06 02:20:28 -0500

    Org-mode and company

--- config/customize.el
+++ config/customize.el
@@ -11,6 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(company-minimum-prefix-length 2)
  '(custom-enabled-themes '(doom-ayu-dark))
  '(custom-safe-themes
    '("631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
---
+++ config/org.el
@@ -0,0 +1,8 @@
+;;;;
+;;;; org.el
+;;;;  by KazaniAvali (2023)
+;;;;
+;;;; Org-mode export configuration.
+;;;;
+
+(setq org-html-validation-link nil)
